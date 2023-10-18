531142fb8f9678af0652d34fff85f11aa0815c2d
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-10-18 22:48:10 +0530

    Add chip element creation with dynamic styles for different values

--- src/components/chips.jsx
+++ src/components/chips.jsx
@@ -1,25 +1,28 @@
-import classNames from 'classnames'
+import classNames from 'classnames';
 
-const chipsSet = {}
+// Initialize an empty object to store each chip's JSX element
+const chipsSet = {};
 
-for(const [ chipsIndex, chipsValue ] of [ 50, 100, 200, 500, 1000, 5000 ].entries()) {
+// Create chip elements for each value
+for (const [chipsIndex, chipsValue] of [50, 100, 200, 500, 1000, 5000].entries()) {
   chipsSet[chipsValue] = <div
-      className='bg-no-repeat bg-[url("/chips.png")] relative'
-      style={ {
-        width: 148, height: 152,
-        backgroundPositionX: -16 - (164 * chipsIndex),
-        backgroundPositionY: -24,
-      } }
-    />
+    className='bg-no-repeat bg-[url("/chips.png")] relative'
+    style={{
+      width: 148, height: 152,
+      backgroundPositionX: -16 - (164 * chipsIndex),
+      backgroundPositionY: -24,
+    }}
+  />
 }
 
-
+// Component to render a chip with a specific value
 export default function Chips({
   className = '',
   value = 50,
   ...props
 }) {
+  // Return the JSX element for the chip with the specified value
   return (
-    <div className={classNames(className)} {...props}>{ chipsSet[value] }</div>
-  )
+    <div className={classNames(className)} {...props}>{chipsSet[value]}</div>
+  );
 }
