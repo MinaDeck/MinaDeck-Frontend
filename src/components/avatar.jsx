48840d0b6dc5ef2b19f72762a1c787ba4a10bdc3
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-10-20 23:21:54 +0530

    Integrated a visual indicator for automatic betting, enhancing the Avatar component to display a robot icon when the user is set to auto-bet

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -6,7 +6,7 @@ const avatarSet = {}
 
 for(let i = 0; i <= 20; i++) {
   const w = (i + 1) % 8
-  const h = Math.floor((i + 1) / 8)
+  const h = Math.floor((i + 1) / 😍
   avatarSet[i] = (
     <div className='absolute inset-0 bg-[url("/avatars.png")] bg-no-repeat rounded-full' style={ {
       backgroundPositionX: -26 - (136 * w),
@@ -45,7 +45,7 @@ export default function Avatar({
   const [ remain, setRemain ] = useState(duration)
   const vvv = useDeferredValue(remain)
   const p = (1 - vvv / duration) * 100
-  const backgroundImage = showCountdown ? `conic-gradient(transparent ${p}%, rgb(234,179,8) ${p}%, rgb(22,163,74) 100%)` : 'none'
+  const backgroundImage = showCountdown ? conic-gradient(transparent ${p}%, rgb(234,179,8) ${p}%, rgb(22,163,74) 100%) : 'none'
   
   useEffect(() => {
     if(showCountdown) {
@@ -65,7 +65,7 @@ export default function Avatar({
     <div data-user-id={userId} className='relative top-6 -translate-x-1/2 -translate-y-1/2 w-24 h-24 box-content p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_1px_1px_rgba(255,255,255,.2),0_0_16px_rgba(0,0,0,.8)]'>
       <div className='rounded-full absolute inset-0 flex items-center justify-center' style={ { backgroundImage } }>
         <div className='relative overflow-hidden w-20 h-20 box-content p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_3px_2px_rgba(255,255,255,.2),0_2px_1px_rgba(255,255,255,.2)]'>
-          { headPic ? <div className='absolute inset-0 bg-center bg-cover bg-no-repeat rounded-full' style={ { backgroundImage: `url("${headPic}")` } } /> : null }
+          { headPic ? <div className='absolute inset-0 bg-center bg-cover bg-no-repeat rounded-full' style={ { backgroundImage: url("${headPic}") } } /> : null }
         </div>
       </div>
       { user?.isAutoBet && showAuto && <img className='absolute -left-2 -top-2 w-8 h-auto' src='/avatar-robot.png' /> }
