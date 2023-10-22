44e1a90da3ab5661eef5667bbce8df7626faa2eb
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-10-22 22:50:28 +0530

    Implement DebuggerLayer component with buttons for game room actions

--- src/components/debugger.jsx
+++ src/components/debugger.jsx
@@ -1,9 +1,11 @@
 export default function DebuggerLayer() {
   return (
     <div className='fixed top-1 left-1 min-w-min bg-white/30 p-4 backdrop-blur-sm'>
+      {/* Button to create a game room */}
       <button type='button'
         className='rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-        onClick={ async () => {
+        onClick={async () => {
+          // Asynchronously send a POST request to create a game room
           const payload = await (await fetch(`http://162.219.87.221/api/game/create`, {
             method: 'POST',
             credentials: 'include',
@@ -13,26 +15,30 @@ export default function DebuggerLayer() {
               topBetChips: 6,
               totalRounds: 100,
             })
-          })).json()
-          console.log(payload)
-        } }
+          })).json();
+          console.log(payload);
+        }}
       >
         Create a game room
       </button>
 
+      {/* Button to enter a game room */}
       <button type='button'
         className='rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-        onClick={ async () => {
-          `ws://162.219.87.221/ws?gameId=d01d687d4bb444e48fcb999facb33db5`
-        } }
+        onClick={async () => {
+          // Placeholder for WebSocket connection (currently does nothing)
+          `ws://162.219.87.221/ws?gameId=d01d687d4bb444e48fcb999facb33db5`;
+        }}
       >
         Enter the room
       </button>
 
+      {/* Button for players to place their bets (no functionality yet) */}
       <button type='button'
         className='rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-      >  Players bet
+      >
+        Players bet
       </button>
     </div>
-  )
+  );
 }
