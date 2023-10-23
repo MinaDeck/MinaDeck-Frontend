b84af0886c7ca25fe074019a364f594959ebf803
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-24 02:28:03 +0530

    Game page

---
+++ src/app/game/page.jsx
@@ -0,0 +1,20 @@
+'use client'
+// import WithSignined from '../with-signin'
+import { useSearchParams } from 'next/navigation'
+import GameRoom from './room'
+import { useGameData } from '@/hooks/useGameData';
+
+
+export default function GamePage() {
+  const searchParams = useSearchParams();
+  const gameId = searchParams.get('gameId');
+  const { gameData } = useGameData();
+  console.log("gameData on game page:",gameId, gameData);
+
+
+  if (!gameId || !/^[a-f\d]{8}$/i.test(gameId)) {
+    return <div className='text-white'>Illegal Room Number {gameId}</div>;
+  }
+
+  return <GameRoom gameId={gameId} />;
+}
