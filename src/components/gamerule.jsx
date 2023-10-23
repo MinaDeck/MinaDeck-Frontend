d80b0cc538557c1ac9195f4787e10dbc48930b0c
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-10-23 23:16:44 +0530

    Implement GameRule component for displaying game rules

--- src/components/gamerule.jsx
+++ src/components/gamerule.jsx
@@ -1,14 +1,13 @@
-import FrameBox from './frame-box'
-import StyledButton from './styled-button'
+import FrameBox from './frame-box';
 
 export default function GameRule({ onClose }) {
   return (
-    <FrameBox
-      onClose={onClose}
-    >
+    <FrameBox onClose={onClose}>
+      {/* Container for the game rule content */}
       <div className='w-[540px] m-10 text-center text-white'>
+        {/* Image representing the game rules */}
         <img className='w-[640px] h-[320px] bg-transparent' src='/maxresdefault.jpg' />
       </div>
     </FrameBox>
-  )
+  );
 }
