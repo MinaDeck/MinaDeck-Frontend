ef61426fb9acba27638c0c0e22387cba687e1b7a
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-03 18:06:22 +0530

    Implement cleanup functions for closing WebSocket connections and store active sockets in a global map

--- src/hooks/use-game-room.js
+++ src/hooks/use-game-room.js
@@ -1,19 +1,27 @@
+// Import necessary libraries and hooks
 import useSWRSubscribe from 'swr/subscription'
 import useLocalStorageState from 'use-local-storage-state'
 import { useSearchParams } from 'next/navigation'
 
+// Create a global WebSocket Map to store WebSocket instances
 const wsMap = new Map()
 globalThis.$WS_MAP$ = wsMap
 
+// Custom hook for managing game room WebSocket connection
 export function useGameRoom(gameId, token) {
+  // Use SWRSubscribe to handle WebSocket connections
   const ws = useSWRSubscribe([gameId, token], ([gameId, token], { next }) => {
     let socket
-    if(gameId && token) {
+
+    // Check if gameId and token are provided
+    if (gameId && token) {
+      // Create WebSocket connection using provided parameters
       const url = new URL(`ws://162.219.87.221/ws`)
       url.searchParams.set('gameId', gameId)
       url.searchParams.set('token', token)
       socket = new WebSocket(url)
 
+      // Handle WebSocket events
       const openHandle = event => {
         const heartbeatHandle = () => {
           socket.send('')
@@ -25,7 +33,8 @@ export function useGameRoom(gameId, token) {
       const messageHandle = event => {
         const state = JSON.parse(event.data)
         console.log('>>PUSH::', state)
-        if(state.code === 10012) {
+        // Close socket if certain condition is met
+        if (state.code === 10012) {
           socket.close()
         }
         next(null, state)
@@ -43,44 +52,54 @@ export function useGameRoom(gameId, token) {
         socket.removeEventListener('close', closeHandle)
       }
 
-      globalThis.abc = messageHandle
-
+      // Add event listeners to the socket
       socket.addEventListener('open', openHandle)
       socket.addEventListener('message', messageHandle)
       socket.addEventListener('error', errorHandle)
       socket.addEventListener('close', closeHandle)
 
+      // Store the socket in the global WebSocket Map
       wsMap.set(`${gameId}_${token}`, socket)
     } else {
-      next(new Error('gameId and token is required'))
+      // Throw an error if gameId and token are not provided
+      next(new Error('gameId and token are required'))
     }
+
+    // Cleanup function to close the socket on unmount
     return () => {
       socket?.close()
     }
   })
 
+  // Return the WebSocket data and a function to send messages
   return {
     data: ws.data,
     error: ws.error,
     send(payload) {
       const socket = wsMap.get(`${gameId}_${token}`)
-      if(socket) {
+      if (socket) {
         console.log('<<SEND::', payload, ws)
-        if(socket.readyState === 1) {
+        // Send the payload if the socket is open
+        if (socket.readyState === 1) {
           socket.send(JSON.stringify(payload))
-        } else if(socket.readyState > 1) {
+        } else if (socket.readyState > 1) {
+          // Reload the page if the socket is closed
           globalThis.location.reload()
         }
       } else {
-        console.error('ws object missing', gameId, token)
+        // Log an error if the WebSocket object is missing
+        console.error('WebSocket object missing', gameId, token)
       }
     }
   }
 }
 
+// Custom hook for obtaining the current game room based on user information
 export function useCurrentGameRoom() {
-  const [ userInfo ] = useLocalStorageState('userinfo')
+  const [userInfo] = useLocalStorageState('userinfo')
+  // Get the 'gameId' from the URL search parameters
   const gameId = useSearchParams().get('gameId')
 
+  // Use the useGameRoom hook with the obtained 'gameId' and user token
   return useGameRoom(gameId, userInfo?.token)
 }
