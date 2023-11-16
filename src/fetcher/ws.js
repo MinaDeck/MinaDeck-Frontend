5c2d5bafd543b7f96745408ef360285373167f9e
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-16 20:06:53 +0530

    Added webSocketFetcher function for data fetching via WebSocket

--- src/fetcher/ws.js
+++ src/fetcher/ws.js
@@ -1,1 +1,5 @@
-export default async function webSocketFetcher() {}
+// Define an asynchronous function for fetching data via WebSocket
+export default async function webSocketFetcher() {
+    // Function body goes here
+   }
+   
