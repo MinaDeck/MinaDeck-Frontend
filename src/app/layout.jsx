0915ec1e9e39da8c64cc51a5503392ab27e3c724
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-20 10:28:33 +0530

    Add global CSS and RootLayout component with background styling

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,22 @@
+// Importing the global CSS styles
 import '@/styles/global.css'
 
+// Metadata object to define the title and description of the application
 export const metadata = {
-  title: 'card poker',
-  description: '...',
+  title: 'card poker', // Title of the application
+  description: '...', // Description of the application, replace '...' with actual description
 }
 
+// The RootLayout component that will wrap around other components in your application
 export default function RootLayout({
-  children
+  children // Props passed to the component, typically the child components
 }) {
   return (
     <html>
-      <body className='bg-cover bg-[url("/bg.png")]'>{children}</body>
+      <body className='bg-cover bg-[url("/bg.png")]'>
+        {/* The children components will be rendered here. The body has a background cover with an image. */}
+        {children}
+      </body>
     </html>
   )
 }
