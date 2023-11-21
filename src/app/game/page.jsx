15d98efe3ef027301d3b0d1772b242abba673c17
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-21 17:08:43 +0530

    Implement GamePage component with URL query param handling

--- src/app/game/page.jsx
+++ src/app/game/page.jsx
@@ -1,20 +1,36 @@
 'use client'
-// import WithSignined from '../with-signin'
-import { useSearchParams } from 'next/navigation'
-import GameRoom from './room'
-import { useGameData } from '@/hooks/useGameData';
+// This marks the component as client-side only
 
+import { useSearchParams } from 'next/navigation'
+// Get access to the search params from the URL
+
+import GameRoom from './room'
+// Import the GameRoom component 
+
+import { useGameData } from '@/hooks/useGameData';
+// Custom hook to get game data
 
 export default function GamePage() {
-  const searchParams = useSearchParams(); 
-  const gameId = searchParams.get('gameId');
-  const { gameData } = useGameData();
-  console.log("gameData on game page:", gameData);
 
+  const searchParams = useSearchParams();
+  // Get the search params object
+  
+  const gameId = searchParams.get('gameId');
+  // Get the gameId from the search params
+  
+  const { gameData } = useGameData();
+  // Get game data from custom hook
+  
+  console.log("gameData on game page:", gameData);
+  // Log the game data
 
   if (!gameId || !/^[a-f\d]{8}$/i.test(gameId)) {
+    // Validate gameId format
+    
     return <div className='text-white'>Illegal Room Number {gameId}</div>;
   }
 
-  return <GameRoom gameId={gameId} />;
+  // If valid, render the GameRoom component
+  return <GameRoom gameId={gameId} />; 
+
 }
