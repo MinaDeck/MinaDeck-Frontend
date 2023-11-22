263d493633f0428b1b4746cc0003e26b41e6a4e4
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-22 18:27:47 +0530

    Add simple Loading component for display during page load

--- src/app/mine/loading.jsx
+++ src/app/mine/loading.jsx
@@ -1,5 +1,8 @@
+// Define a functional component named 'Loading'
 export default function Loading() {
+  // The component returns a simple message indicating a loading state
   return (
+    // React Fragment shorthand <> is used to return the enclosed message
     <>Page is loading</>
   )
 }
