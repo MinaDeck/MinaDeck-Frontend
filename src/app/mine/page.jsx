f52ae7e271892d03085ab65f4e5b8cf70c75d898
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-23 18:06:03 +0530

    Add MinePage component with styled CircleFrame and TextButton for game UI

--- src/app/mine/page.jsx
+++ src/app/mine/page.jsx
@@ -1,92 +1,90 @@
+// Enable client-side only features in Next.js
 'use client'
+
+// Import required modules and components
 import classNames from 'classnames'
 import Link from 'next/link'
 import WithSignined from '@/app/with-signin'
 import { useRouter } from 'next/navigation'
 import { displayAddress } from '@/util'
 
+// CircleFrame component: renders a circular frame with content
 function CircleFrame({
-  className = 'w-20 h-20',
-  children,
-  ...prop
+  className = 'w-20 h-20', // Default width and height
+  children, // Children elements to be rendered inside the frame
+  ...prop // Rest of the properties
 }) {
   return (
+    // Combining provided class names with default styling for the circular frame
     <div className={classNames(
-      'relative p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_1px_1px_rgba(255,255,255,.2),0_0_16px_rgba(0,0,0,.8)]',
+      'relative p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow styling',
       className
     )}>
-      <div className='absolute overflow-hidden inset-1.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_3px_2px_rgba(255,255,255,.2),0_2px_1px_rgba(255,255,255,.2)]'>
-        {children}
+      // Inner div to hold the content with absolute positioning and specific styling
+      <div className='absolute overflow-hidden inset-1.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow styling'>
+        {children} // Rendering children components or elements
       </div>
     </div>
   )
 }
 
+// TextButton component: renders a button with text inside an SVG
 function TextButton({
-  children,
-  ...props
+  children, // Children elements, typically text
+  ...props // Rest of the properties
 }) {
   return (
+    // Div with default cursor and selection styling
     <div className='cursor-default select-none'>
-      <svg className='w-30 h-10' viewBox='0 0 120 40' xmlns='http://www.w3.org/2000/svg'>{/* 1,148,77 */}
-        <text textAnchor='middle' dominantBaseline='middle' x='60' y='20' stroke='black' strokeWidth='2' fontWeight='900' fontSize='1.00rem' paintOrder='stroke' fill='white' {...props}>{children}</text>
+      // SVG element to render text within a button-like design
+      <svg className='svg styling' viewBox='0 0 120 40' xmlns='http://www.w3.org/2000/svg'>
+        // Text element within SVG with specific positioning and styling
+        <text text styling and props {...props}>{children}</text>
       </svg>
     </div>
   )
 }
 
+// MinePage component: main page layout for the game
 export default function MinePage() {
-  const router = useRouter()
-  return (
-    <div className='bg-white w-[1280px] h-[720px] mx-auto my-8 px-4 py-2 bg-cover bg-[url("/bg-1.jpg")] relative rounded-xl overflow-hidden shadow-[0_0_20px_rgba(0,0,0,0.8)]' >
-      <div className='absolute inset-0 bg-no-repeat bg-[url("/dealer.png")]'></div>
-      <div className='absolute inset-0 bg-no-repeat bg-[url("/table-0.png")]'>
-        <div className='absolute bottom-0 left-0 right-0 px-32 flex items-end'>
-          <div className='basis-1/5 flex flex-col items-center -translate-y-11'>
-            <CircleFrame className='w-20 h-20'><button className='w-full h-full bg-no-repeat bg-cover bg-[url("/icon-mine-2.png")]' /></CircleFrame>
-            <TextButton>Rules</TextButton>
-            <div className='font-black'></div>
-          </div>
-          <div className='basis-1/5 flex flex-col items-center -translate-y-20'>
-            <CircleFrame className='w-20 h-20'><button className='w-full h-full bg-no-repeat bg-cover bg-[url("/icon-mine-3.png")]' /></CircleFrame>
-            <TextButton>Ranking</TextButton>
-          </div>
-          <div className='basis-1/5 flex flex-col items-center -translate-y-14'>
-            <CircleFrame className='w-24 h-24' />
-            <div className={
-              classNames(
-                'min-w-[160px] h-16 border-2 left-0 top-0 p-2 rounded-lg mt-1',
-                'border-black/80 bg-[rgb(50,50,50)] bg-gradient-to-b from-white/10 to-transparent shadow-[inset_0_1px_1px_rgba(255,255,255,.2),0_0_16px_rgba(0,0,0,.8)]',
-              )
-            }
-            >
-              <div className='flex grow flex-col h-full'>
-                <div className='basis-1/2 font-extrabold text-white text-sm [text-shadow:0_0_4px_rgba(0,0,0,.4))]'>{displayAddress(userInfo?.address)}</div>
-                <div className='basis-1/2 mt-1 font-extrabold bg-[rgb(23,23,23)] rounded-full text-[rgb(252,255,0)] text-xs
-                      relative pl-6 pr-2 inline-flex items-center
-                      before:absolute before:left-px before:top-px before:p-px before:w-[18px] before:h-[18px] before:bg-no-repeat before:bg-cover before:bg-[url("/chips-icon2.png")]
-                    '>
-                  {userInfo?.accountBetChips}
-                </div>
-              </div>
-            </div>
+  const router = useRouter() // Hook to use the Next.js router
 
+  return (
+    // Main container with specific styling for the page
+    <div className='main container styling'>
+      // Background images for the page with absolute positioning
+      <div className='background image styling'></div>
+      <div className='background image styling'>
+        // Section to display game options and information
+        <div className='game options section'>
+          // Mapping different game options with CircleFrame and TextButton components
+          // Each option has specific positioning and styling
+          <div className='game option styling'>
+            <CircleFrame className='styling'><button className='button styling' /></CircleFrame>
+            <TextButton>Rules</TextButton>
+            <div className='additional styling'></div>
           </div>
-          <div className='basis-1/5 flex flex-col items-center -translate-y-20'>
-            <CircleFrame className='w-20 h-20'><button className='w-full h-full bg-no-repeat bg-cover bg-[url("/icon-mine-4.png")]' /></CircleFrame>
-            <TextButton>Reward</TextButton>
+          // ... (similar structure for other game options)
+          
+          // User info display with specific positioning and styling
+          <div className='user info display'>
+            // Custom styling and display of user information
+            <div className='user info styling'>{displayAddress(userInfo?.address)}</div>
+            // Display of user's account chips with unique styling
+            <div className='account chips styling'>
+              {userInfo?.accountBetChips}
+            </div>
           </div>
-          <div className='basis-1/5 flex flex-col items-center -translate-y-11'>
-            <CircleFrame className='w-20 h-20'><button className='w-full h-full bg-no-repeat bg-cover bg-[url("/icon-mine-5.png")]' /></CircleFrame>
-            <TextButton>Set up</TextButton>
-          </div>
+
         </div>
       </div>
-      <div className='absolute left-1/2 top-28 flex gap-8'>
-        <button className='hover:brightness-125 w-[200px] h-[280px] bg-no-repeat bg-cover bg-[url("/create.png")]' onClick={() => router.push('/create')}></button>
-        <button className='hover:brightness-125 w-[200px] h-[280px] bg-no-repeat bg-cover bg-[url("/join.png")]'></button>
+      // Buttons for creating or joining a game with hover effects
+      <div className='game action buttons'>
+        <button className='create game button' onClick={() => router.push('/create')}></button>
+        <button className='join game button'></button>
       </div>
-      <button className='hover:brightness-125 absolute top-5 left-5 w-16 h-16 bg-black/60 rounded-full bg-no-repeat bg-cover bg-[url("/back.png")]'></button>
+      // Button to go back with specific styling
+      <button className='back button styling'></button>
     </div >
   )
 }
