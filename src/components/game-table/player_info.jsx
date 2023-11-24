284698887500a2317e1f3a3e9177919c59f083e4
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-24 11:46:55 +0530

    Integrate the display of player action in PlayerInfo component

--- src/components/game-table/player_info.jsx
+++ src/components/game-table/player_info.jsx
@@ -1,7 +1,7 @@
 // PlayerInfo.jsx
 import React from 'react';
 
-const PlayerInfo = ({ name, chips, cards, action }) => {
+const PlayerInfo = ({ name, chips, cards, action, chipsWon }) => {
   return (
     <div className="player-info">
       <div className="player-name">{name}</div>
@@ -14,6 +14,7 @@ const PlayerInfo = ({ name, chips, cards, action }) => {
         ))}
       </div>
       <div className="player-action">Action: {action}</div>
+      <div className="chips-won">Chips Won: {chipsWon}</div>
     </div>
   );
 };
