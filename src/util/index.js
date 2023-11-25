63a6550a8aa03c2229af4bd6edbbade885a49cd7
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-25 16:46:19 +0530

    Implement base58 encoding/decoding and add utility functions for address display and dollar formatting

--- src/util/index.js
+++ src/util/index.js
@@ -1,23 +1,23 @@
 // Importing the 'bs58' library for base58 encoding and decoding
 import bs58 from 'bs58';
 
-// Constant representing the character map for base58 encoding
+// Character map for base58 encoding, excluding characters that might look similar
 const BASE58_CHAR_MAP = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 
 // Function to encode a string using base58
 export const encodeBs58 = (str) => {
-  // Convert the string to base58
+  // First convert the string to base58
   const base58str = stringToBase58(str);
-  // Convert the base58 string to an integer
+  // Then convert the base58 string to an integer
   const int = base58ToInt(base58str);
   return int;
 };
 
 // Function to decode an integer using base58
 export const decodeBs58 = (int) => {
-  // Convert the integer to a base58 string
+  // Convert the integer back to a base58 string
   const bs58str = intToBase58(BigInt(int));
-  // Convert the base58 string to a regular string
+  // Convert the base58 string back to the original string
   const str = base58ToString(bs58str);
   return str;
 };
@@ -25,13 +25,13 @@ export const decodeBs58 = (int) => {
 // Function to convert an integer to a base58 string
 function intToBase58(int) {
   let base58 = '';
-  // Iterate until the integer is greater than zero
+  // Iterating over the integer value to construct a base58 string
   while (int > 0n) {
-    // Calculate the remainder when dividing by 58
+    // Calculate remainder when dividing by 58
     const remainder = Number(int % 58n);
-    // Append the corresponding character to the base58 string
+    // Append corresponding character to base58 string from the character map
     base58 = BASE58_CHAR_MAP[remainder] + base58;
-    // Update the integer by dividing by 58
+    // Divide integer by 58 for next iteration
     int = int / 58n;
   }
   return base58;
@@ -40,24 +40,23 @@ function intToBase58(int) {
 // Function to convert a base58 string to an integer
 function base58ToInt(base58) {
   return base58
-    // Convert the string to an array of characters, reverse it
     .split('')
-    .reverse()
-    // Calculate the integer value using the base58 character map
+    .reverse() // Reversing the string for correct calculation
+    // Reduce the array to calculate the integer value from base58
     .reduce((value, char, index) => value + BigInt(BASE58_CHAR_MAP.indexOf(char)) * 58n ** BigInt(index), BigInt(0))
     .toString();
 }
 
 // Function to convert a base58 string to a regular string
 function base58ToString(base58) {
-  // Decode the base58 string to bytes and then decode the bytes to a string
+  // Decode base58 string into bytes and then convert bytes to a string
   const bytes = bs58.decode(base58);
   return new TextDecoder().decode(bytes);
 }
 
 // Function to convert a string to base58
 function stringToBase58(str) {
-  // Encode the string to bytes and then encode the bytes to base58
+  // Convert string into bytes and then encode bytes into base58
   const bytes = new TextEncoder().encode(str);
   return bs58.encode(bytes);
 }
@@ -66,10 +65,10 @@ function stringToBase58(str) {
 export const formatDollar = (value) => {
   const numericValue = Number(value);
   if (Number.isNaN(numericValue)) {
-    // If the value is not numeric, return the original value
+    // Return original value if it's not a numeric value
     return value;
   } else {
-    // If the value is numeric, format it as a dollar amount
+    // Format the numeric value as a dollar amount
     return Intl.NumberFormat('en-US', {
       notation: 'standard',
     }).format(numericValue);
@@ -81,5 +80,6 @@ export const optionSeparator = -&&&&&-;
 
 // Function to display a truncated version of an address
 export function displayAddress(address = '') {
+  // Truncate and format the address for display
   return address?.slice(0, 4) + '...' + address?.slice(-8);
 }
