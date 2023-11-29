a5b0d82f56d10775566b48e5f3e87c9153a4de50
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-29 23:25:35 +0530

    Introduce GameRule component for displaying game rules

--- src/components/gamerule.jsx
+++ src/components/gamerule.jsx
@@ -1,11 +1,12 @@
 import FrameBox from './frame-box';
 
+// GameRule component - displays the rules of the game
 export default function GameRule({ onClose }) {
+  // Renders a FrameBox component which acts as a container for the game rules
   return (
-    <FrameBox onClose={onClose}>
-      {/* Container for the game rule content */}
-      <div className='w-[540px] m-10 text-center text-white'>
-        {/* Image representing the game rules */}
+    <FrameBox onClose={onClose}> {/* FrameBox used for presenting the content, with a prop to handle closure */}
+      <div className='w-[540px] m-10 text-center text-white'> {/* Styling for the container of the game rules content */}
+        {/* Image tag used to display the game rules. The source of the image is '/maxresdefault.jpg' */}
         <img className='w-[640px] h-[320px] bg-transparent' src='/maxresdefault.jpg' />
       </div>
     </FrameBox>
