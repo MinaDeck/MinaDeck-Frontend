5b3c991e49d63318a521a05a8ad267bc929c8fcd
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-30 21:41:50 +0530

    Implement ShareLink1 component for streamlined match link sharing

--- src/components/share-link1.jsx
+++ src/components/share-link1.jsx
@@ -3,39 +3,48 @@ import StyledButton from './styled-button'
 import { useEffect, useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
+// ShareLink1 component - used for sharing a match link
 export default function ShareLink1({ onClose }) {
-  const [ url, setUrl ] = useState(null)
-  const [ copyed, setCopyed ] = useState(false)
+  const [ url, setUrl ] = useState(null) // State to store the URL to be shared
+  const [ copyed, setCopyed ] = useState(false) // State to track if the URL has been copied
 
   useEffect(() => {
+    // Effect to set the URL when the component mounts
     const currentUrl = new URL(location.href)
     const shareUrl = new URL(location.pathname, location.origin)
+    // Adding 'gameId' query parameter to the URL
     shareUrl.searchParams.set('gameId', currentUrl.searchParams.get('gameId'))
-    setUrl(shareUrl.toString())
+    setUrl(shareUrl.toString()) // Setting the formatted URL
   }, [])
 
+  // Main return method that renders the share link UI
   return (
     <FrameBox
-      title={<div className='bg-[url("/title-share.png")] bg-no-repeat bg-top h-[96px] -translate-y-1/2'></div>}
-      onClose={onClose}
+      title={<div className='title-styling'></div>} // Title bar with a background image
+      onClose={onClose} // onClose prop for closing the component
     >
-      <div className='w-[540px] m-10 text-center text-white'>
-        <h4 className='text-3xl font-black'>Match created!</h4>
-        <p>Your match is ready to go!</p>
-        <p>Share it with your friends using the link below.</p>
-        <p><img className='inline-block' src='/share-link-icon.png'/></p>
-        <p>Only players with this code are able <br/>to join the match.</p>
-        <p className='text-[#fff000]'>
-          <CopyToClipboard text={url}
-            onCopy={() => { setCopyed(true); setTimeout(() => setCopyed(false), 3000) } }>
-            <a className='underline cursor-pointer'>{url}</a>
+      <div className='content-container'>
+        <h4 className='heading'>Match created!</h4>
+        <p>Instructions for sharing the match.</p>
+        <p><img className='icon' src='/share-link-icon.png'/></p>
+        <p>Information about the match accessibility.</p>
+        <p className='share-link'>
+          {/* Copy to clipboard functionality for the match URL */}
+          <CopyToClipboard text={url} onCopy={() => {
+            setCopyed(true); 
+            setTimeout(() => setCopyed(false), 3000) // Reset the copied state after 3 seconds
+          }}>
+            <a className='link'>{url}</a> {/* Displaying the match URL */}
           </CopyToClipboard>
         </p>
       </div>
-      <div className='flex justify-center relative'>
-        <CopyToClipboard text={url}
-          onCopy={() => { setCopyed(true); setTimeout(() => setCopyed(false), 3000) } }>
-          <StyledButton className='bg-[#ff9000] m-2' roundedStyle='rounded-full'><div className='text-2xl'>{copyed ? 'COPYED' : 'COPY LINK' }</div></StyledButton>
+      <div className='button-container'>
+        {/* Button to copy the match link */}
+        <CopyToClipboard text={url} onCopy={() => {
+          setCopyed(true);
+          setTimeout(() => setCopyed(false), 3000)
+        }}>
+          <StyledButton className='button-style'><div className='button-text'>{copyed ? 'COPYED' : 'COPY LINK' }</div></StyledButton>
         </CopyToClipboard>
       </div>
     </FrameBox>
