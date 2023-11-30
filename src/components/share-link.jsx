7e02ba6c47ca21a6a0fcf87648f7d546be3e235d
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-11-30 21:34:25 +0530

    Implement ShareLink component for sharing match links

--- src/components/share-link.jsx
+++ src/components/share-link.jsx
@@ -5,41 +5,47 @@ import { useState, useEffect } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { DocumentDuplicateIcon } from '@heroicons/react/24/outline'
 
+// ShareLink component - used for sharing a match link
 export default function ShareLink({ onClose, link }) {
   const router = useRouter()
   const [ url, setUrl ] = useState(null)
   const [ copyed, setCopyed ] = useState(false)
 
   useEffect(() => {
+    // On component mount, the URL is constructed and set
     const currentUrl = new URL(link, location.origin)
-
     setUrl(currentUrl.toString())
   }, [])
+
+  // Main return method that renders the share link UI
   return (
     <FrameBox
-      title={<div className='bg-[url("/title-share.png")] bg-no-repeat bg-top h-[96px] -translate-y-1/2'></div>}
-      onClose={onClose}
-      showClose={false}
+      title={<div className='title-styling'></div>} // Title bar with a background image
+      onClose={onClose} // onClose prop for closing the component
+      showClose={false} // Option to hide the close button
     >
-      <div className='w-[560px] m-10 text-center text-white'>
-        <h4 className='text-3xl font-black'>Match created!</h4>
-        <p>Your match is ready to go!</p>
-        <p>Share it with your friends using the link below.</p>
-        <p><img className='inline-block' src='/share-link-icon.png'/></p>
-        <p>Only players with this code are able <br/>to join the match.</p>
-        <p className='text-[#fff000] cursor-pointer'>
-          <CopyToClipboard text={url}
-            onCopy={() => { setCopyed(true); setTimeout(() => setCopyed(false), 3000) } }>
-            <a className='relative underline'>
-              {url}
-              { copyed && <DocumentDuplicateIcon className='absolute -right-5 top-1 w-4 h-4' /> }
+      <div className='content-container'>
+        <h4 className='heading'>Match created!</h4>
+        <p>Instructions for sharing the match.</p>
+        <p><img className='icon' src='/share-link-icon.png'/></p>
+        <p>Information about the match accessibility.</p>
+        <p className='share-link'>
+          {/* Copy to clipboard functionality for the match URL */}
+          <CopyToClipboard text={url} onCopy={() => {
+            setCopyed(true); 
+            setTimeout(() => setCopyed(false), 3000) // Reset the copied state after 3 seconds
+          }}>
+            <a className='link'>
+              {url} {/* Displaying the match URL */}
+              { copyed && <DocumentDuplicateIcon className='icon' /> } {/* Icon changes when URL is copied */}
             </a>
           </CopyToClipboard>
         </p>
       </div>
-      <div className='flex justify-center'>
-        <StyledButton className='bg-[#ff9000] m-2' roundedStyle='rounded-full' onClick={ () => { router.push(link) } }>
-          <div className='text-2xl' >LET'S GO</div>
+      <div className='button-container'>
+        {/* Button to start the game or go to the match */}
+        <StyledButton className='button-style' onClick={ () => { router.push(link) } }>
+          <div className='button-text'>LET'S GO</div>
         </StyledButton>
       </div>
     </FrameBox>
