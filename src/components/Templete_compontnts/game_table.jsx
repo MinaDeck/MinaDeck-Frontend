fd19236260e9fadd228eb66fdd0c708741fe7eba
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-09 13:21:32 +0530

    Refactor GameTable.jsx for improved conciseness and readability

--- src/components/Templete_compontnts/game_table.jsx
+++ src/components/Templete_compontnts/game_table.jsx
@@ -1,47 +1,37 @@
-// GameTable.jsx
 import React from 'react';
 import PlayerInfo from './PlayerInfo';
 import WinningBoard from './WinningBoard';
 
-const GameTable = ({ players, cards, winners, leaderboard }) => {
-  return (
-    <div className="game-table">
-      <WinningBoard winners={winners} />
+const GameTable = ({ players, cards, winners, leaderboard }) => (
+  <div className="game-table">
+    <WinningBoard winners={winners} />
 
-      {/* Display player information and cards */}
-      {players.map((player, index) => (
-        <PlayerInfo
-          key={index}
-          name={player.name}
-          chips={player.chips}
-          cards={player.cards}
-          action={player.action}
-          chipsWon={player.chipsWon}
-        />
-      ))}
+    {/* Display player information and cards */}
+    {players.map((player, index) => (
+      <PlayerInfo key={index} {...player} />
+    ))}
 
-      {/* Display chips won in the leaderboard */}
-      <div className="leaderboard">
-        <h2>Leaderboard</h2>
-        <ul>
-          {leaderboard.map((entry, index) => (
-            <li key={index}>
-              {entry.player}: {entry.chipsWon} Chips
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* Display cards on the table */}
-      <div className="table-cards">
-        {cards.map((card, index) => (
-          <div key={index} className="card">
-            {card}
-          </div>
+    {/* Display chips won in the leaderboard */}
+    <div className="leaderboard">
+      <h2>Leaderboard</h2>
+      <ul>
+        {leaderboard.map((entry, index) => (
+          <li key={index}>
+            {entry.player}: {entry.chipsWon} Chips
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
-  );
-};
+
+    {/* Display cards on the table */}
+    <div className="table-cards">
+      {cards.map((card, index) => (
+        <div key={index} className="card">
+          {card}
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default GameTable;
