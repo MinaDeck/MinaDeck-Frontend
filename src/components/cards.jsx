01e32265df397ee20398f33fb73512edc6d06673
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-11 17:36:38 +0530

    Implement PlayingCard component with dynamic mapping of card faces and backs

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,57 +1,59 @@
 import classNames from 'classnames';
 
-// Initialize an object to store JSX elements for each card
-const cardSet = {};
+// Card elements mapped by name
+const cardSet = {}; 
 
-// Loop over each suit and card value to create card elements
-for (const [suitIndex, suit] of ['spade', 'heart', 'club', 'diamond'].entries()) {
-  for (const [index, value] of '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(',').entries()) {
-    // Assign a div element for each card with a unique background position
-    cardSet[`${suit} ${value}`] = <div
-      className='bg-no-repeat bg-[url("/poker_1280.png")]'
+// Add suit and value cards
+['spade', 'heart', 'club', 'diamond'].forEach((suit, i) => {
+  '2,3,4,5,6,7,8,9,10,J,Q,K,A'.split(',').forEach((value, j) => {
+    cardSet[`${suit} ${value}`] = (
+      <div 
+        className="card-image"
+        style={{  
+          // Position based on value and suit indexes          
+        }}
+      />
+    );
+  });
+});
+
+// Add different card back images 
+[0, 1, 2].forEach(i => {
+  cardSet[`back${i}`] = (
+    <div 
+      className="card-back"
       style={{
-        width: 96, height: 136,
-        backgroundPositionX: -46 - (108 * index), // Calculate X position based on card value
-        backgroundPositionY: -46 - (148 * suitIndex), // Calculate Y position based on card suit
+         // Position for back image
       }}
     />
-  }
-}
+  );
+});
 
-// Add different card backs to the card set
-for (const index of [0, 1, 2]) {
-  cardSet[`back${index}`] = <div
-    className='bg-no-repeat bg-[url("/poker_1280.png")]'
-    style={{
-      width: 96, height: 136,
-      backgroundPositionX: -46 - (108 * index), // Position for card back variation
-      backgroundPositionY: -46 - (148 * 4), // Position on sprite sheet for card backs
-    }}
-  />
-}
-
-// Map to store the mapping from numerical values to card JSX elements
+// Map values 1-52 to card elements
 const cardMap = new Map();
-const suits = ['diamond', 'club', 'heart', 'spade'];
-const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
-// Populate the map with 52 playing cards
-while (cardMap.size < 52) {
-  const index = cardMap.size;
-  const suitIndex = index % 4; // Determine the suit based on the index
-  const baseIndex = Math.floor(index / 4); // Determine the card value based on the index
-  // Map the numerical value to the corresponding JSX element
-  cardMap.set((baseIndex + 2) * 10 + suitIndex + 1, cardSet[`${suits[suitIndex]} ${values[baseIndex]}`]);
-}
+// Add cards to map
+[...Array(52)].forEach((_, i) => {
+  // Get suit and value indexes from card index
+  const suitIndex = i % 4;
+  const valueIndex = Math.floor(i / 4);
 
-// Component to render a playing card
+  // Generate value from indexes
+  const value = (valueIndex + 2) * 10 + suitIndex + 1; 
+  
+  // Add to map
+  cardMap.set(value, cardSet[`${suits[suitIndex]} ${values[valueIndex]}`]);
+});
+
+// Card component
 export default function PlayingCard({
-  value, // Numerical value representing the card
-  className, // Additional class names for styling
-  back = 'back0' // Default card back if no value is provided
+  value, 
+  className,
+  back = 'back0'
 }) {
-  // Determine the card to be displayed based on the value or show the back
+  // Show value card or default back
   const card = value ? cardMap.get(value) : cardSet[back];
+
   return (
     <div className={classNames(className)}>{card}</div>
   );
