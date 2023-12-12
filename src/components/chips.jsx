1c1356e93c79624bfb5533bc4cffa43aabcd6933
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-12 22:00:23 +0530

    Implement reusable Chips component with visual customization

--- src/components/chips.jsx
+++ src/components/chips.jsx
@@ -1,27 +1,28 @@
 import classNames from 'classnames';
 
-// Initialize an empty object to store each chip's JSX element
+// Initialize an object to store JSX elements for each chip value
 const chipsSet = {};
 
-// Create chip elements for each value
+// Loop over each chip value to create chip elements
 for (const [chipsIndex, chipsValue] of [50, 100, 200, 500, 1000, 5000].entries()) {
+  // Assign a div element for each chip with a unique background position
   chipsSet[chipsValue] = <div
     className='bg-no-repeat bg-[url("/chips.png")] relative'
     style={{
       width: 148, height: 152,
-      backgroundPositionX: -16 - (164 * chipsIndex),
-      backgroundPositionY: -24,
+      backgroundPositionX: -16 - (164 * chipsIndex), // X position for each chip value
+      backgroundPositionY: -24, // Y position is constant for all chips
     }}
   />
 }
 
-// Component to render a chip with a specific value
+// Chips component to render a chip with a specific value
 export default function Chips({
-  className = '',
-  value = 50,
+  className = '', // Additional class names passed as props
+  value = 50, // Default value of the chip
   ...props
 }) {
-  // Return the JSX element for the chip with the specified value
+  // Render the JSX element for the chip with the specified value
   return (
     <div className={classNames(className)} {...props}>{chipsSet[value]}</div>
   );
