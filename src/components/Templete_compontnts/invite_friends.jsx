18893f699bc566e75bf860487d066119e118ea7b
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-15 13:30:13 +0530

    Implement Invite Friends feature for game

--- src/components/Templete_compontnts/invite_friends.jsx
+++ src/components/Templete_compontnts/invite_friends.jsx
@@ -1,25 +1,31 @@
-// InviteFriends.jsx (or a relevant component)
 import React, { useState } from 'react';
 
 const InviteFriends = () => {
+  // State for the invite link
   const [inviteLink, setInviteLink] = useState('');
 
+  // Function to generate a unique invite link
   const generateInviteLink = () => {
     // Implement the logic to generate a unique invite link for the user.
     // This might involve creating a unique code or token associated with the user.
 
-
     const randomCode = Math.random().toString(36).substring(7);
     const generatedLink = `https://yourgame.com/invite/${randomCode}`;
 
+    // Set the generated link in the state
     setInviteLink(generatedLink);
   };
 
   return (
     <div>
+      {/* Invite Friends Section */}
       <h2>Invite Friends</h2>
+
+      {/* Display the invite link */}
       <p>Share the following link with your friends to invite them to the game:</p>
       <p>{inviteLink}</p>
+
+      {/* Button to generate a new invite link */}
       <button onClick={generateInviteLink}>Generate Invite Link</button>
     </div>
   );
