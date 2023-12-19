7c13145e82bcaad63a1ebf39c3fc72ec459f0084
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-19 12:53:47 +0530

    Implement SVGText component for enhanced text rendering and styling

--- src/components/svg-text.jsx
+++ src/components/svg-text.jsx
@@ -1,26 +1,28 @@
 import { useEffect, useRef } from 'react';
 
+// SVGText component: Renders text within an SVG for enhanced styling and effects
 export default function SVGText({
   children,
-  fontSize = '1.25rem', // Default font size
+  fontSize = '1.25rem', // Default font size for the text
   ...props
 }) {
-  const textRef = useRef(null); // Reference for the text element
-  const svgRef = useRef(null); // Reference for the SVG element
+  const textRef = useRef(null); // useRef hook to reference the text element
+  const svgRef = useRef(null); // useRef hook to reference the SVG element
 
   useEffect(() => {
-    // Adjust the size of the SVG to match the text size
+    // Adjusts the SVG size to match the size of the text
     if (textRef.current) {
       const { width, height } = textRef.current.getBoundingClientRect();
+      // Dynamically sets the SVG dimensions based on text size
       svgRef.current.style.cssText += `;width:${width}px;height:${height}px;`;
     }
-  }, [textRef.current]); // Dependency on the text element reference
+  }, [textRef.current]); // Effect runs when the text element reference updates
 
   return (
     <svg ref={svgRef} viewBox='0 0 120 40' xmlns='http://www.w3.org/2000/svg'>
-      {/* Text element with customizable properties */}
+      {/* Text element within SVG, customizable via props */}
       <text ref={textRef} textAnchor='left' dominantBaseline='hanging' x='0' y='0' stroke='white' strokeWidth='2' fontSize={fontSize} fontWeight='900' paintOrder='stroke' fill='currentColor' {...props}>
-        {children}
+        {children} {/* Displaying children elements or text */}
       </text>
     </svg>
   );
