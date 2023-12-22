5cf8aea38b1e6fd6de30f5a49cb51b4b4786f5d8
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-22 12:28:46 +0530

    Enhance PlayerInfo component with card images, status, and total value

--- src/components/Templete_compontnts/player_info.jsx
+++ src/components/Templete_compontnts/player_info.jsx
@@ -1,20 +1,39 @@
-// PlayerInfo.jsx
 import React from 'react';
 
 const PlayerInfo = ({ name, chips, cards, action, chipsWon }) => {
+  // Calculate the total value of the player's cards
+  const totalCardValue = cards.reduce((total, card) => total + card.value, 0);
+
+  // Determine the player's status based on their action
+  const playerStatus = action === 'Fold' ? 'Folded' : 'Active Player';
+
+  // Display the card images instead of simple text
+  const cardImages = cards.map((card, index) => (
+    <img key={index} className="card-image" src={`images/${card.suit}_${card.rank}.png`} alt={`${card.rank} of ${card.suit}`} />
+  ));
+
   return (
     <div className="player-info">
+      {/* Player Name */}
       <div className="player-name">{name}</div>
+
+      {/* Player Chips */}
       <div className="player-chips">Chips: {chips}</div>
-      <div className="player-cards">
-        {cards.map((card, index) => (
-          <div key={index} className="card">
-            {card}
-          </div>
-        ))}
-      </div>
+
+      {/* Player Cards */}
+      <div className="player-cards">{cardImages}</div>
+
+      {/* Player Action */}
       <div className="player-action">Action: {action}</div>
+
+      {/* Chips Won */}
       <div className="chips-won">Chips Won: {chipsWon}</div>
+
+      {/* Total Card Value */}
+      <div className="total-card-value">Total Card Value: {totalCardValue}</div>
+
+      {/* Player Status */}
+      <div className="player-status">Status: {playerStatus}</div>
     </div>
   );
 };
