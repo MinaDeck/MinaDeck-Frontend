d3136efc9baede8c9f1e6cb0671f50cf9ec9125f
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-26 22:04:56 +0530

    Implement UserDashboard with login check and daily reward feature

--- src/components/Templete_compontnts/daily_login_reward.jsx
+++ src/components/Templete_compontnts/daily_login_reward.jsx
@@ -1,52 +1,68 @@
-// UserDashboard.jsx (or a relevant component)
-import React, { useState, useEffect } from 'react';
+// UserDashboard.jsx
+
+import React, { useState, useEffect } from 'react'; 
 
 const UserDashboard = () => {
+
+  // State to track if user is logged in
   const [isLogged, setIsLogged] = useState(false);
+
+  // State to track if daily reward is already claimed
   const [dailyRewardClaimed, setDailyRewardClaimed] = useState(false);
 
   useEffect(() => {
-    // Check if the user is logged in (you might use authentication state)
-    // For example, you can use AuroWallet authentication.
-    // Assume isLogged is set based on the authentication status.
-    // This could be part of a larger authentication system.
 
-    // For demonstration purposes, let's simulate a daily login check.
+    // Check authentication status on mount and set isLogged
+    // This could be part of a larger auth system
+
+    // Check if daily reward already claimed today
     const lastLoginDate = localStorage.getItem('lastLoginDate');
     const today = new Date().toISOString().split('T')[0];
 
     if (lastLoginDate === today) {
-      setDailyRewardClaimed(true);
+      // If last login was today, set reward as already claimed
+      setDailyRewardClaimed(true); 
     } else {
+      // Otherwise set as not claimed yet
       setDailyRewardClaimed(false);
     }
+
   }, [isLogged]);
 
   const claimDailyReward = () => {
-    // Perform the necessary actions to grant the daily reward to the user.
-    // This might include updating their balance or giving them an in-game item.
+    
+    // Logic to grant reward to user
+    // e.g. update balance or grant item
 
-    // For demonstration purposes, let's update the last login date in local storage.
-    localStorage.setItem('lastLoginDate', new Date().toISOString().split('T')[0]);
+    // Update last login date
+    localStorage.setItem('lastLoginDate', new Date().toISOString().split('T')[0]);  
 
+    // Set state to reflect reward has been claimed
     setDailyRewardClaimed(true);
   };
 
   return (
     <div>
       <h2>User Dashboard</h2>
+
       {isLogged ? (
         <div>
-          <p>Welcome back! </p>
+          <p>Welcome back!</p>
+
+          {/* Show appropriate message if reward already claimed */}
           {dailyRewardClaimed ? (
-            <p>Daily reward already claimed today.</p>
+            <p>Daily reward already claimed today.</p>  
           ) : (
+            // Allow claiming reward if not already claimed
             <button onClick={claimDailyReward}>Claim Daily Reward</button>
           )}
-        </div>
+
+        </div>  
       ) : (
+        // Display message if user not logged in
         <p>Please log in to access your dashboard.</p>
       )}
+
     </div>
   );
 };
