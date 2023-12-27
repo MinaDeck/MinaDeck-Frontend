e8ceea5d2f50b144a8d8c4b884cae85295e0dda6
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-27 17:24:07 +0530

    Introduce GameControls for player actions and chat

--- src/components/Templete_compontnts/game_control.jsx
+++ src/components/Templete_compontnts/game_control.jsx
@@ -1,18 +1,32 @@
 // GameControls.jsx
+
 import React from 'react';
 
 const GameControls = ({ onAction, onChatOpen }) => {
-  // Render game controls for actions like bet, raise, check, fold, and chat.
+
+  // Render game control buttons for player actions
+
   return (
     <div className="game-controls">
+      
+      {/* Bet button */}
       <button onClick={() => onAction('bet')}>Bet</button>
+
+      {/* Raise button */}
       <button onClick={() => onAction('raise')}>Raise</button>
+
+      {/* Check button */}
       <button onClick={() => onAction('check')}>Check</button>
+
+      {/* Fold button */}
       <button onClick={() => onAction('fold')}>Fold</button>
+
+      {/* Show cards button */}
       <button onClick={() => onAction('showCards')}>Show Cards</button>
       
-      {/* Button to open chat */}
-      <button onClick={onChatOpen}>Chat with fellow players </button>
+      {/* Chat button */}
+      <button onClick={onChatOpen}>Chat with fellow players</button>
+
     </div>
   );
 };
