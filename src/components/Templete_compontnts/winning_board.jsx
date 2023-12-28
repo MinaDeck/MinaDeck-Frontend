31e1a6135d6e89125fec70d41339194806005ed5
Saurus9290 <103920942+Saurus9290@users.noreply.github.com>
2023-12-28 14:36:55 +0530

    Add WinningBoard component to display winners and congratulatory messages

--- src/components/Templete_compontnts/winning_board.jsx
+++ src/components/Templete_compontnts/winning_board.jsx
@@ -1,15 +1,34 @@
-// WinningBoard.jsx
 import React from 'react';
 
 const WinningBoard = ({ winners }) => {
+  // Calculate the total number of winners
+  const totalWinners = winners.length;
+
+  // Display a message based on the number of winners
+  const winnersMessage =
+    totalWinners === 0
+      ? 'No winners yet.'
+      : totalWinners === 1
+      ? 'Winner:'
+      : 'Winners:';
+
   return (
     <div className="winning-board">
-      <div className="winners">Winners:</div>
+      {/* Winners Message */}
+      <div className="winners-message">{winnersMessage}</div>
+
+      {/* List of Winners */}
       <ul>
         {winners.map((winner, index) => (
           <li key={index}>{winner}</li>
         ))}
       </ul>
+
+      {/* Total Number of Winners */}
+      <div className="total-winners">Total Winners: {totalWinners}</div>
+
+      {/* Congratulations Message */}
+      {totalWinners > 0 && <div className="congratulations">Congratulations to the winners!</div>}
     </div>
   );
 };
