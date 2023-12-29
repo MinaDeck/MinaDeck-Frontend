9e4281afb62f7bab49e15b32dad17d94a1acf2ba
0xshikhar <101751723+0xKryptonian@users.noreply.github.com>
2023-12-29 11:44:41 +0530

    added websocket fetching function

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -1,81 +1,81 @@
 // Import state management hooks
-import stateFetcher from "@/fetcher/state" 
+import stateFetcher from "@/fetcher/state"
 import { useSpringRef, useTransition } from "@react-spring/web"
 import { useState, useEffect, useDeferredValue } from 'react'
 import useSWR from 'swr'
 
 // Avatar image set 
-const avatarSet = {} 
+const avatarSet = {}
 
-for(let i = 0; i <= 20; i++) {
- const w = (i + 1) % 8
- const h = Math.floor((i + 1) / 8)
- avatarSet[i] = (
-   <div className='absolute inset-0 bg-[url("/avatars.png")] bg-no-repeat rounded-full' style={ {
-     backgroundPositionX: -26 - (136 * w), 
-     backgroundPositionY: -26 - (136 * h),
-   } } />
- )
+for (let i = 0; i <= 20; i++) {
+  const w = (i + 1) % 8
+  const h = Math.floor((i + 1) / 8)
+  avatarSet[i] = (
+    <div className='absolute inset-0 bg-[url("/avatars.png")] bg-no-repeat rounded-full' style={{
+      backgroundPositionX: -26 - (136 * w),
+      backgroundPositionY: -26 - (136 * h),
+    }} />
+  )
 }
 
 // Avatar component
 export default function Avatar({
- userId, // User ID
- user, // User object
- headPic = '', // Custom user headshot
- dealer = false, // Whether this is the dealer
- showCountdown = false, // Show countdown timer
- startTime = 1, 
- endTime = 0,
- hiascent = false, // Show Hiascent logo
- showAuto = true, // Show robot icon for autoplay
- children,
+  userId, // User ID
+  user, // User object
+  headPic = '', // Custom user headshot
+  dealer = false, // Whether this is the dealer
+  showCountdown = false, // Show countdown timer
+  startTime = 1,
+  endTime = 0,
+  hiascent = false, // Show Hiascent logo
+  showAuto = true, // Show robot icon for autoplay
+  children,
 }) {
 
- // Countdown timer
- const duration = endTime - startTime
- const [ remain, setRemain ] = useState(duration)
- const vvv = useDeferredValue(remain)
- const p = (1 - vvv / duration) * 100
- const backgroundImage = showCountdown ? `conic-gradient(transparent ${p}%, rgb(234,179,8) ${p}%, rgb(22,163,74) 100%)` : 'none'
+  // Countdown timer
+  const duration = endTime - startTime
+  const [remain, setRemain] = useState(duration)
+  const vvv = useDeferredValue(remain)
+  const p = (1 - vvv / duration) * 100
+  const backgroundImage = showCountdown ? `conic-gradient(transparent ${p}%, rgb(234,179,8) ${p}%, rgb(22,163,74) 100%)` : 'none'
 
- useEffect(() => {
-   if(showCountdown) {
-     requestAnimationFrame(() => {
-       const now = Date.now()
-       if(endTime > now) {
-         setRemain(endTime - now) 
-       } else {
-         setRemain(0)
-       }
-     })
-   }
- }, [vvv, showCountdown, duration, endTime, startTime])
+  useEffect(() => {
+    if (showCountdown) {
+      requestAnimationFrame(() => {
+        const now = Date.now()
+        if (endTime > now) {
+          setRemain(endTime - now)
+        } else {
+          setRemain(0)
+        }
+      })
+    }
+  }, [vvv, showCountdown, duration, endTime, startTime])
 
- return (
-   // Avatar container
-   <div data-user-id={userId} className='relative top-6 -translate-x-1/2 -translate-y-1/2 w-24 h-24 box-content p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_1px_1px_rgba(255,255,255,.2),0_0_16px_rgba(0,0,0,.8)]'>
-     
+  return (
+    // Avatar container
+    <div data-user-id={userId} className='relative top-6 -translate-x-1/2 -translate-y-1/2 w-24 h-24 box-content p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_1px_1px_rgba(255,255,255,.2),0_0_16px_rgba(0,0,0,.8)]'>
+
      // Countdown circle
-     <div className='rounded-full absolute inset-0 flex items-center justify-center' style={ { backgroundImage } }>
-       
+      <div className='rounded-full absolute inset-0 flex items-center justify-center' style={{ backgroundImage }}>
+
        // Avatar image or placeholder
-       <div className='relative overflow-hidden w-20 h-20 box-content p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_3px_2px_rgba(255,255,255,.2),0_2px_1px_rgba(255,255,255,.2)]'>
-         { headPic ? <div className='absolute inset-0 bg-center bg-cover bg-no-repeat rounded-full' style={ { backgroundImage: url("${headPic}") } } /> : null }
-       </div>
-       
-     </div>
-     
+        <div className='relative overflow-hidden w-20 h-20 box-content p-0.5 border-2 border-black/80 bg-[rgb(50,50,50)] rounded-full shadow-[inset_0_3px_2px_rgba(255,255,255,.2),0_2px_1px_rgba(255,255,255,.2)]'>
+          {headPic ? <div className='absolute inset-0 bg-center bg-cover bg-no-repeat rounded-full' style={{ backgroundImage: url("${headPic}") }} /> : null}
+        </div>
+
+      </div>
+
      // Auto play icon
-     { user?.isAutoBet && showAuto && <img className='absolute -left-2 -top-2 w-8 h-auto' src='/avatar-robot.png' /> }
-     
+      {user?.isAutoBet && showAuto && <img className='absolute -left-2 -top-2 w-8 h-auto' src='/avatar-robot.png' />}
+
      // Dealer icon
-     { dealer && <img className='absolute -right-2 -top-2' src='/dealer-icon.png' /> }
-     
-     // Hiascent logo 
-     { hiascent && <img className='absolute top-0 left-0' src='hiascent.png' /> }
-     
-     { children }
-   </div>
- )
+      {dealer && <img className='absolute -right-2 -top-2' src='/dealer-icon.png' />}
+
+     // Hiascent logo
+      {hiascent && <img className='absolute top-0 left-0' src='hiascent.png' />}
+
+      {children}
+    </div>
+  )
 }
--- src/fetcher/ws.js
+++ src/fetcher/ws.js
@@ -1,4 +1,48 @@
 // Define an asynchronous function for fetching data via WebSocket
+
 export default async function webSocketFetcher() {
-    // Function body goes here
+    // WebSocket endpoint URL
+    const webSocketUrl = 'wss://42.43.54.33/poker';
+
+    // Create a WebSocket connection
+    const webSocket = new WebSocket(webSocketUrl);
+
+    // Define a promise to handle the WebSocket connection
+    const webSocketPromise = new Promise((resolve, reject) => {
+        // Event handler for successful WebSocket connection
+        webSocket.onopen = () => {
+            console.log('WebSocket connection established.');
+            resolve(webSocket);
+        };
+
+        // Event handler for WebSocket errors
+        webSocket.onerror = (error) => {
+            console.error('WebSocket error:', error);
+            reject(error);
+        };
+    });
+
+    try {
+        // Wait for the WebSocket connection to be established
+        const socket = await webSocketPromise;
+
+        // Event handler for receiving messages from the WebSocket server
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            console.log('Received data from WebSocket:', data);
+
+            // Process the received data as needed
+        };
+
+        // Event handler for WebSocket closure
+        socket.onclose = (event) => {
+            console.log('WebSocket connection closed:', event);
+
+            // Handle WebSocket closure, such as attempting to reconnect
+        };
+
+    } catch (error) {
+        // Handle any errors that occurred during WebSocket setup
+        console.error('Error setting up WebSocket:', error);
+    }
 }
