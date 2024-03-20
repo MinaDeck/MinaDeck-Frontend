267a003042c8381580fd16374c6ee9e8d4aa0644
saurabh <sauraj9290@gmail.com>
2024-03-20 20:23:31 +0530

    fixed prisma dependency issue

--- src/util/db.js
+++ src/util/db.js
@@ -1,5 +1,5 @@
 // This file should have a .js or .jsx extension
-const { PrismaClient } = require('@prisma/client');
+const {PrismaClient} = require('@prisma/client');
 
 const prisma = global.prisma || new PrismaClient();
 
