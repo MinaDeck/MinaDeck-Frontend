6ed05cb1c0aa6dc943c2764173209f6eddce296a
saurabh <sauraj9290@gmail.com>
2024-04-14 22:31:30 +0530

    handled errors in database functions

--- src/util/databaseFunctions.js
+++ src/util/databaseFunctions.js
@@ -1,101 +1,80 @@
 import axios from "axios";
 
 export const checkAddress = async (addressToCheck) => {
-    try {
-
-        const response = await axios.post('/api/check-address', { address: addressToCheck[0] });
-        const data = response.data;
-
-        if (data.response.length > 0) {
-            return true;
-        } else {
-            return false;
-        }
-
-    } catch (error) {
-        console.error('error:', error);
-        return false;
-    }
+  try {
+    const response = await axios.post('/api/check-address', { address: addressToCheck[0] });
+    const data = response.data;
+    return data.response.length > 0;
+  } catch (error) {
+    console.error('Error checking address:', error);
+    throw error;
+  }
 };
 
 export const getUserData = async (address) => {
-    try {
-
-        const response = await axios.post('/api/get-user', { address: address[0] });
-        const data = response.data;
-
-        return data;
-
-    } catch (error) {
-        console.error('error:', error);
-        return error
-    }
+  try {
+    const response = await axios.post('/api/get-user', { address: address[0] });
+    const data = response.data;
+    return data;
+  } catch (error) {
+    console.error('Error getting user data:', error);
+    throw error;
+  }
 };
 
-
 export const addPlayerData = async (address, formData) => {
-    try {
-
-        const response = await axios.post('/api/add-user',
-            {
-                address: address,
-                name: formData.name,
-                userName: formData.userName
-            }
-        );
-        const data = response.data;
-
-        if (!data?.success) {
-            throw new Error('Error adding player data. API response is not successful.');
-        }
-
-        return true; // Return true on successful addition
+  try {
+    const response = await axios.post('/api/add-user', {
+      address: address,
+      name: formData.name,
+      userName: formData.userName
+    });
+    const data = response.data;
+    if (!data?.success) {
+      throw new Error('Error adding player data. API response is not successful.');
     }
-    catch (error) {
-        console.error('error:', error);
-        throw error;
-    }
-}
-
+    return true;
+  } catch (error) {
+    console.error('Error adding player data:', error);
+    throw error;
+  }
+};
 
 export const createPokerGame = async (id, size, lowBetChips, topBetChips, totalRounds) => {
-    try {
-
-        const response = await axios.post('/api/create-game',
-            {
-                gameId: id,
-                size: size,
-                lowBetChips: lowBetChips,
-                topBetChips: topBetChips,
-                totalRounds: totalRounds
-            }
-        );
-        const data = response.data;
-
-        if (!data?.success) {
-            throw new Error('Error adding player data. API response is not successful.');
-        }
-
-        return true; // Return true on successful addition
+  try {
+    const response = await axios.post('/api/create-game', {
+      gameId: id,
+      size: size,
+      lowBetChips: lowBetChips,
+      topBetChips: topBetChips,
+      totalRounds: totalRounds
+    });
+    const data = response.data;
+    if (!data?.success) {
+      throw new Error('Error creating poker game. API response is not successful.');
     }
-    catch (error) {
-        console.error('error:', error);
-        throw error;
-    }
-}
+    return true;
+  } catch (error) {
+    console.error('Error creating poker game:', error);
+    throw error;
+  }
+};
 
-
-// adding 1 user at time to the particular game on Mina Poker
 export const addPlayerToGame = async (gameId, address, userAddress) => {
-    try {
-
-        if (error) {
-            throw error;
-        }
-
-        console.log("game players added", data)
+  try {
+    const response = await axios.post('/api/add-player-to-game', {
+      gameId: gameId,
+      address: address,
+      userAddress: userAddress
+    });
+    const data = response.data;
+    if (!data?.success) {
+      throw new Error('Error adding player to game. API response is not successful.');
     }
-    catch (error) {
-        console.error('error:', error);
-    }
-}
+    console.log("Player added to the game:", data);
+    return true;
+  } catch (error) {
+    console.error('Error adding player to game:', error);
+    throw error;
+  }
+};
